3fc6892b5eea9b5abab9e35d9f0bfd94968e7a0d
julianajlk <julianajlk@gmail.com>
2024-11-04 14:46:28 -0500

    feat: Att getCorrelationID helper

--- src/cohesion/helpers.js
+++ src/cohesion/helpers.js
@@ -1,7 +1,35 @@
+import Cookies from 'universal-cookie';
+import { v4 as uuidv4 } from 'uuid';
 import { EventMap } from './constants';
 
 /**
- * Submit ('beam') an event via Tagular to Make
+ * Fetch or Create a Tagular CorrelationID. This also refreshes the cookie's expiry.
+ */
+export const getCorrelationID = () => {
+  const COOKIE_NAME = 'tglr_correlation_id';
+  const PARAM_NAME = 'correlationId';
+
+  function getQueryParameter(name) {
+    const params = new URLSearchParams(window.location.search);
+
+    return params.get(name);
+  }
+
+  let paramId = getQueryParameter(PARAM_NAME) || new Cookies().get(COOKIE_NAME);
+
+  if (!paramId) {
+    paramId = uuidv4();
+  }
+
+  const expirationDate = new Date();
+  expirationDate.setMinutes(expirationDate.getMinutes() + 30); // 30 mins expiration from now
+  new Cookies().set(COOKIE_NAME, paramId, { expires: expirationDate });
+
+  return paramId;
+};
+
+/**
+ * Submit ('beam') an event via Tagular to Make.
  * @param eventName Schema Name of the Event
  * @param eventData The data required by the schema
  */
